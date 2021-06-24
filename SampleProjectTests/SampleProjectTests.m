3e33c95f93f8f89e7ca79bb7f3a611d3460fc878
Shakir <shakir@evincedev.com>
2021-06-24 10:58:11 +0530

    Test changes

--- SampleProjectTests/SampleProjectTests.m
+++ SampleProjectTests/SampleProjectTests.m
@@ -24,7 +24,7 @@
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
-    NSLog(@"APP TESTING DONE...");
+    NSLog(@"APP TESTING DONE.....");
 }
 
 - (void)testPerformanceExample {
